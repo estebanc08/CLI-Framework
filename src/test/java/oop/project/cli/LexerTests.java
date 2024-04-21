84422c7d215e52b4a4350682fad541ead92259fc
[ripeliz] <elizabethchoy02@gmail.com>
2024-04-21 03:28:40 -0400

    simple test namedArgs test cases

--- src/test/java/oop/project/cli/LexerTests.java
+++ src/test/java/oop/project/cli/LexerTests.java
@@ -128,8 +128,26 @@ public class LexerTests {
     }
 
     @Nested
-    class NamedArgTests{
-        //TODO: ADD BASIC TEST CASES FOR ARGUMENTS
+    class NamedArgTests {
+
+        @ParameterizedTest
+        @MethodSource
+        public void testNamedArgs(String name, String command, ArrayList<ArgToken> expected) {
+            test(command, expected);
+        }
+
+        public static Stream<Arguments> testNamedArgs() {
+            return Stream.of(
+                    Arguments.of("Single Named Argument", "--name=\"value\"", new ArrayList<>(List.of(
+                            new ArgToken(ArgToken.Type.NAMED_ARG, "name", new ArrayList<>(List.of("value")))))),
+                    Arguments.of("Multiple Named Arguments", "--name1=\"value1\" --name2=2", new ArrayList<>(List.of(
+                            new ArgToken(ArgToken.Type.NAMED_ARG, "name1", new ArrayList<>(List.of("value1"))),
+                            new ArgToken(ArgToken.Type.NAMED_ARG, "name2", new ArrayList<>(List.of(new BigInteger("2"))))))),
+                    Arguments.of("Invalid Named Argument", "--=value", null),
+                    Arguments.of("Named Argument with Array Value", "--name3=[1 2 3 4]", new ArrayList<>(List.of(
+                            new ArgToken(ArgToken.Type.NAMED_ARG, "name3", new ArrayList<>(List.of(new BigInteger("1"), new BigInteger("2"), new BigInteger("3"), new BigInteger("4")))))))
+            );
+        }
     }
 
     private static void test(String command, ArrayList<ArgToken> expected )  {
